8e3c78768f27bfebb88e08b9bd01987b32f334fd
Gustavo Arellano <gustavo813arellano@gmail.com>
2020-03-23 23:36:14 -0700

    updated item collection

--- models/item.js
+++ models/item.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 
 var ItemSchema = new mongoose.Schema({
-    type: {type: String, require: true},
-    image: []
+    length = 50,
+    chunckSize = 50,
+    filename = String,
+    contentType = String,
+    aliases = [],
+    metadata = null,
 }, {timestamps: true});
 
 mongoose.model('Item', ItemSchema)
